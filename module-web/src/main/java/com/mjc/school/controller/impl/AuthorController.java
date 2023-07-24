0d50a9cfa217b3ca7e7a3a3dc8f063962722f3f7
Dmitry Golub <dmgolub@mail.ru>
2023-07-24 18:54:16 +0400

    CommandHandler annotation added

--- module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
@@ -1,6 +1,7 @@
 package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
+import com.mjc.school.controller.annotation.CommandHandler;
 import com.mjc.school.service.BaseService;
 import com.mjc.school.service.dto.AuthorRequestDto;
 import com.mjc.school.service.dto.AuthorResponseDto;
@@ -18,26 +19,31 @@ public class AuthorController implements BaseController<AuthorRequestDto, Author
 	}
 
 	@Override
+	@CommandHandler("Create author")
 	public AuthorResponseDto create(final AuthorRequestDto request) {
 		return authorService.create(request);
 	}
 
 	@Override
+	@CommandHandler("Get all authors")
 	public List<AuthorResponseDto> readAll() {
 		return authorService.readAll();
 	}
 
 	@Override
+	@CommandHandler("Get author by id")
 	public AuthorResponseDto readById(final Long id) {
 		return authorService.readById(id);
 	}
 
 	@Override
+	@CommandHandler("Update author by id")
 	public AuthorResponseDto update(final AuthorRequestDto request) {
 		return authorService.update(request);
 	}
 
 	@Override
+	@CommandHandler("Delete author by id")
 	public boolean deleteById(final Long id) {
 		return authorService.deleteById(id);
 	}
--- module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
@@ -1,6 +1,7 @@
 package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
+import com.mjc.school.controller.annotation.CommandHandler;
 import com.mjc.school.service.BaseService;
 import com.mjc.school.service.dto.NewsRequestDto;
 import com.mjc.school.service.dto.NewsResponseDto;
@@ -18,26 +19,31 @@ public class NewsController implements BaseController<NewsRequestDto, NewsRespon
 	}
 
 	@Override
+	@CommandHandler("Create news")
 	public NewsResponseDto create(final NewsRequestDto request) {
 		return newsService.create(request);
 	}
 
 	@Override
+	@CommandHandler("Get all news")
 	public List<NewsResponseDto> readAll() {
 		return newsService.readAll();
 	}
 
 	@Override
+	@CommandHandler("Get news by id")
 	public NewsResponseDto readById(final Long id) {
 		return newsService.readById(id);
 	}
 
 	@Override
+	@CommandHandler("Update news by id")
 	public NewsResponseDto update(final NewsRequestDto request) {
 		return newsService.update(request);
 	}
 
 	@Override
+	@CommandHandler("Delete news by id")
 	public boolean deleteById(final Long id) {
 		return newsService.deleteById(id);
 	}
